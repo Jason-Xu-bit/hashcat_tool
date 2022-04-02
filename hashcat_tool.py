4cf1b5515756bdb27d91fc5a552cbb9821396a20
Jason-Xu-bit <jxj20071019@163.com>
2022-04-02 12:04:04 +0800

    V1.2.0 添加暴力破解模式

--- hashcat_tool.py
+++ hashcat_tool.py
@@ -8,27 +8,35 @@ dic_cmd = ''  # 格式化的字典命令
 w = ''  # 性能
 
 ErrP('w', '请将本程序放在hashcat目录下')
-ErrP('i', '可多选文件')
+ErrP('i', '可多选文件，如果不选择字典文件，则只使用暴力破解')
 
+ErrP('p','选择hashcat文件：')
 file_path = opfiles(title='打开hashcat文件')  # 读取hashcat文件
+ErrP('p','选择字典文件：')
 dic_path = opfiles(title='打开字典文件')  # 读取字典文件
-
-for i in range(len(dic_path)):
-    dic_cmd = dic_cmd + ' ' + dic_path[i]
 
 w = input('性能(1-4)>')
 
-if input('是否在字典破解完成后开始8位数字暴力破解(默认:y/n)>') == 'n':
+if not dic_path:
     for i in range(len(file_path)):  # 破解命令
-        ErrP('p', '\n===============' + '[' + str(i + 1) + '/' + str(len(file_path)) + '] ' + file_path[i].split('/')[-1] + ' [字典破解]===============')  # 进度
-        ErrP('g', 'hashcat -m 22000 -w ' + w + ' ' + file_path[i] + ' ' + dic_cmd)
-        system('hashcat -m 22000 -w ' + w + ' ' + file_path[i] + ' ' + dic_cmd)
-else:
-    for i in range(len(file_path)):  # 破解命令
-        ErrP('p', '\n===============' + '[' + str(i + 1) + '/' + str(len(file_path)) + '] ' + file_path[i].split('/')[-1] + ' [字典破解]===============')  # 进度
-        ErrP('g', 'hashcat -m 22000 -w ' + w + ' ' + file_path[i] + ' ' + dic_cmd)
-        system('hashcat -m 22000 -w ' + w + ' ' + file_path[i] + ' ' + dic_cmd)
-        ErrP('p', '\n===============' + '[' + str(i + 1) + '/' + str(len(file_path)) + '] ' + file_path[i].split('/')[-1] + ' [暴力破解]===============')  # 进度
+        ErrP('p', '\n\a===============' + '[' + str(i + 1) + '/' + str(len(file_path)) + '] ' + file_path[i].split('/')[-1] + ' [暴力破解]===============')  # 进度
         ErrP('g', 'hashcat -a 3 -m 22000 -w ' + w + ' ' + file_path[i] + ' ?d?d?d?d?d?d?d?d')
         system('hashcat -a 3 -m 22000 -w ' + w + ' ' + file_path[i] + ' ?d?d?d?d?d?d?d?d')
+else:
+    for i in range(len(dic_path)):
+        dic_cmd = dic_cmd + ' ' + dic_path[i]
+
+    if input('是否在字典破解完成后开始8位数字暴力破解(默认:y/n)>') == 'n':
+        for i in range(len(file_path)):  # 破解命令
+            ErrP('p', '\n\a===============' + '[' + str(i + 1) + '/' + str(len(file_path)) + '] ' + file_path[i].split('/')[-1] + ' [字典破解]===============')  # 进度
+            ErrP('g', 'hashcat -m 22000 -w ' + w + ' ' + file_path[i] + ' ' + dic_cmd)
+            system('hashcat -m 22000 -w ' + w + ' ' + file_path[i] + ' ' + dic_cmd)
+    else:
+        for i in range(len(file_path)):  # 破解命令
+            ErrP('p', '\n\a===============' + '[' + str(i + 1) + '/' + str(len(file_path)) + '] ' + file_path[i].split('/')[-1] + ' [字典破解]===============')  # 进度
+            ErrP('g', 'hashcat -m 22000 -w ' + w + ' ' + file_path[i] + ' ' + dic_cmd)
+            system('hashcat -m 22000 -w ' + w + ' ' + file_path[i] + ' ' + dic_cmd)
+            ErrP('p', '\n\a===============' + '[' + str(i + 1) + '/' + str(len(file_path)) + '] ' + file_path[i].split('/')[-1] + ' [暴力破解]===============')  # 进度
+            ErrP('g', 'hashcat -a 3 -m 22000 -w ' + w + ' ' + file_path[i] + ' ?d?d?d?d?d?d?d?d')
+            system('hashcat -a 3 -m 22000 -w ' + w + ' ' + file_path[i] + ' ?d?d?d?d?d?d?d?d')
 input('...')
